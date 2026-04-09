a95d4edb82500799adf09cd4ecce3083b4917b04
Harunzy <Harunzy55@gmail.com>
2026-04-09 12:05:21 -0700

    add quote counter

--- BreakinBad/index.js
+++ BreakinBad/index.js
@@ -3,13 +3,18 @@ const quoteCounterEle = document.querySelector(".quote-counter");
 const quoteBtnEle = document.querySelector(".quote-btn");
 const authorEle = document.querySelector(".author");
 
+let counter = 0;
+
 async function getQuoteFromAPI() {
   const res = await fetch("https://api.breakingbadquotes.xyz/v1/quotes");
   const data = await res.json();
   const quote = data[0].quote;
   const author = data[0].author;
+
   displayQuoteEle.textContent = quote;
   authorEle.textContent = author;
+  counter += 1;
+  quoteCounterEle.textContent = `You've read ${counter} quotes!`;
 }
 quoteBtnEle.addEventListener("click", () => {
   getQuoteFromAPI();
