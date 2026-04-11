538253568ab8fd27106a5b3bf43548fa3cfb72f7
Harunzy <Harunzy55@gmail.com>
2026-04-10 20:12:15 -0700

    add import pizza data

--- pizza-menu/src/components/Menu.js
+++ pizza-menu/src/components/Menu.js
@@ -1,29 +1,29 @@
+import { pizzaData } from "../data";
 import Pizza from "./Pizzas";
 function Menu() {
+  console.log(pizzaData);
   return (
     <div>
       <section className="menu">
         <h2>Our Menu</h2>
         <p>Check our delicious pizzas below</p>
         <div>
-          <Pizza
+          {pizzaData.map((item, index) => {
+            <div key={index}>
+              <Pizza
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.photoName}
+              />
+            </div>;
+          })}
+          {/* <Pizza
             name="Focaccia"
             description="Delicious Italian flatbread topped wiith olive oil and herbs."
             price="$10"
             image="/pizzas/focaccia.jpg"
-          />
-          <Pizza
-            name="Pizza Spinaci"
-            description="Fresh Spinach, garlic, and mozzarella on a cripspy crust."
-            price="13"
-            image="pizzas/spinaci.jpg"
-          />
-          <Pizza
-            name="Pizza Salamino"
-            description="Spicy salami, tomato sauce, and mozzarella cheese."
-            price="$9"
-            image="pizzas/focaccia.jpg"
-          />
+          /> */}
         </div>
       </section>
     </div>
