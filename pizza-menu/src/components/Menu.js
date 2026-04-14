fceefb0bebf930418307d6f211a80eaa1fd9337e
Harunzy <Harunzy55@gmail.com>
2026-04-14 14:27:32 -0700

    add new class

--- pizza-menu/src/components/Menu.js
+++ pizza-menu/src/components/Menu.js
@@ -7,15 +7,16 @@ function Menu() {
       <section className="menu">
         <h2>Our Menu</h2>
         <p>Check our delicious pizzas below</p>
-        <div>
+        <div className="grid-container">
           {pizzaData.map((item, index) => (
-            <div key={index}>
+            <div key={index} className="grid-item">
               <Pizza
                 name={item.name}
                 description={item.ingredients}
                 price={item.price}
                 image={item.photoName}
               />
+              <button className="grid-btn">Buy</button>
             </div>
           ))}
         </div>
