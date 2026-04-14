2b872061743ba23eabe9f71f0193bd1e257b88d6
Harunzy <Harunzy55@gmail.com>
2026-04-13 22:07:59 -0700

    add

--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -1,7 +1,16 @@
-export default function Square(props){
-    return(
-        <div>
-            <button className="square">{props.title}</button>
-        </div>
-    )
+import { useState } from "react";
+
+export default function Square({ value, onSquareClick }) {
+  //   const [value, setValue] = useState(null);
+
+  //   function handleClick() {
+  //     setValue("X");
+  //   }
+  return (
+    <div>
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
+    </div>
+  );
 }
