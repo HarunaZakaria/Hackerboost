a48735faecc362bb5ce4d656ed1e9091213d53f8
Harunzy <Harunzy55@gmail.com>
2026-04-14 23:03:06 -0700

    add data.js

---
+++ products/src/data.js
@@ -0,0 +1,8 @@
+const products = [
+  { category: "Fruits", price: "Ghc10", stock: true, name: "apple" },
+  { category: "Fruits", price: "Ghc20", stock: false, name: "Orange" },
+  { category: "Fruits", price: "Ghc10", stock: true, name: "Mango" },
+  { category: "Vegetables", price: "Ghc20", stock: true, name: "Cabbage" },
+  { category: "Vegetables", price: "Ghc40", stock: true, name: "Green Beens" },
+  { category: "Vegetables", price: "Ghc10", stock: false, name: "Tomato" },
+];
