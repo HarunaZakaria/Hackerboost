45666410b91151e0a5895fe99e52d455761fe803
Harunzy <Harunzy55@gmail.com>
2026-04-16 11:10:47 -0700

    add

--- products/src/components/FilterableProductTable.jsx
+++ products/src/components/FilterableProductTable.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import ProductTable from "./ProductTable";
 import SearchBar from "./Search";
 
 export default function FilterableProductTable({ products }) {
+  const [filterTex, setFilterText] = useState("Fruits");
+  const [inStockOnly, setInStockOnly] = useState(false);
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar filterTex={filterTex} inStockOnly={inStockOnly} />
+      <ProductTable
+        products={products}
+        filterTex={filterTex}
+        inStockOnly={inStockOnly}
+      />
     </div>
   );
 }
--- products/src/components/Search.jsx
+++ products/src/components/Search.jsx
@@ -1,4 +1,4 @@
-export default function SearchBar() {
+export default function SearchBar({ filterText, inStockOnly }) {
   return (
     <div>
       <form action="#" className="form">
@@ -6,10 +6,11 @@ export default function SearchBar() {
           type="text"
           name="search-text"
           placeholder="Search... "
+          value={filterText}
           required
         />
         <div className="check-box">
-          <input type="checkbox" />
+          <input type="checkbox" checked={inStockOnly} />
           <p>Only show products in stock</p>
         </div>
       </form>
