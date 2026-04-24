e44922a0a605b2ef1022d2d2cc7b0b54600e994e
Harunzy <Harunzy55@gmail.com>
2026-04-24 13:52:12 -0700

    add

--- courses/src/App.jsx
+++ courses/src/App.jsx
@@ -1,122 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
+const courses = [
+  {
+    name: "Frontend Development",
+    description: "responsible for creating the client side",
+    price: 50,
+  },
+  {
+    name: "Backend Development",
+    description: "responsible for creating the server side logic",
+    price: 100,
+  },
+  {
+    name: "Full StackDeevelopment",
+    description: "responsible for combining both frontend and backend",
+    price: 200,
+  },
+];
 function App() {
-  const [count, setCount] = useState(0)
+  const [steps, setSteps] = useState(0);
+
+  function handleFrontendChange() {
+    setSteps(1);
+  }
+  function handleBackendChange() {
+    setSteps(2);
+  }
+
+  function handleFullStackChange() {
+    setSteps(3);
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div>
+      <div className="container">
+        <div className="container-btns">
+          <div
+            className={steps === 1 ? "active" : "neutral"}
+            onClick={handleFrontendChange}
+          >
+            Frontend
+          </div>
+          <div
+            className={steps === 2 ? "active" : "neutral"}
+            onClick={handleBackendChange}
+          >
+            Backend
+          </div>
+          <div
+            className={steps === 3 ? "active" : "neutral"}
+            onClick={handleFullStackChange}
+          >
+            FullStack
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+        <div className="container-content">
+          {courses.map((course, index) => (
+            <div key={index}>
+              <h1>{course.name}</h1>
+              <p>{course.description}</p>
+              <p>{course.price}</p>
+            </div>
+          ))}
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
