b2960838747c8d5cd419c5f2b060ebd7d37642be
Harunzy <Harunzy55@gmail.com>
2026-04-25 22:34:54 -0700

    add form comp

---
+++ travel/src/components/Form.jsx
@@ -0,0 +1,12 @@
+function Form() {
+  return (
+    <div>
+      <form>
+        <h3>What do you need for you trip?</h3>
+        <select></select>
+        <input type="text" placeholder="Item...." />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
