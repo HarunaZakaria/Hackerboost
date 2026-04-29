ddb35dc6ac2827eefbf095a31c8b4dfc50f35b73
Harunzy <Harunzy55@gmail.com>
2026-04-29 02:02:23 -0700

    add a solution

---
+++ flash-card/src/Question.jsx
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    id: 1,
+    question: "What is useState?",
+    answer: "A React Hook for state",
+  },
+  {
+    id: 2,
+    question: "What is useEffect?",
+    answer: "A React Hook for effect",
+  },
+  {
+    id: 3,
+    question: "What is useMemo?",
+    answer: "A React Hook for memory",
+  },
+];
+
+export default questions;
