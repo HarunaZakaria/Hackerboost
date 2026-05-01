ae4a276eb97ffba270999852fa7b6f29e5ba2641
Harunzy <Harunzy55@gmail.com>
2026-04-30 20:28:44 -0700

    add

--- QuoteSpark/src/components/List.jsx
+++ QuoteSpark/src/components/List.jsx
@@ -1,23 +1,40 @@
+import { useState } from "react";
 import people from "../data";
 import GetImgUrl from "../utils";
 
 export default function List() {
-  const listItem = people.map((person) => {
-    <li key={person.id}>
-      <img src={GetImgUrl} alt={person.name} />
-      <p>
-        <b>{person.name}: </b>
-        {" " + person.profession + " "}
-        Known for {person.accomplishment}
-      </p>
-    </li>;
-  });
+  const [specialist, setSpecialist] = useState("");
+
+  //handle chemist click
+  function handleChemist() {
+    const name = "chemist";
+    setSpecialist(name);
+    console.log(specialist);
+  }
+  //handle mathematician click
+  function handleMathematician() {
+    setSpecialist("mathematician");
+    console.log(specialist);
+  }
+  //handle Phsicians click
+  function handlePhsician() {
+    setSpecialist("Phsician");
+    console.log(specialist);
+  }
   return (
     <div>
       <h1>Scientist</h1>
+      <div className="specilist-btns">
+        <button onClick={handleChemist}>Chemist</button>
+        <button onClick={handleMathematician}>Mathematicians</button>
+        <button onClick={handlePhsician}>Phsicians</button>
+      </div>
       <ul>
         {people.map((person) => (
-          <li key={person.id}> {person.name}</li>
+          <div key={person.id}>
+            <h2 key={person.id}> {person.name}</h2>
+            <p>{person.profession}</p>
+          </div>
         ))}
       </ul>
     </div>
