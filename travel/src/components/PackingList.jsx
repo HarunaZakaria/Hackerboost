fefbb796934357c1b4221a1c9ff8cbce37f07965
Harunzy <Harunzy55@gmail.com>
2026-05-07 23:45:58 -0700

    add onClearList props

--- travel/src/components/PackingList.jsx
+++ travel/src/components/PackingList.jsx
@@ -1,14 +1,25 @@
 import Item from "./Item";
 
-export default function PackingList({ items , deleteItem, onToggle}) {
+export default function PackingList({
+  items,
+  deleteItem,
+  onToggle,
+  onClearList,
+}) {
   //console.log("This is from Packing list" + newItem);
   return (
     <div className="list">
       <ul>
         {items.map((item, index) => (
-          <Item item={item} key={index} deleteItem={deleteItem} onToggle={onToggle}/>
+          <Item
+            item={item}
+            key={index}
+            deleteItem={deleteItem}
+            onToggle={onToggle}
+          />
         ))}
       </ul>
+      <button onClick={onClearList}>Clear List</button>
     </div>
   );
 }
