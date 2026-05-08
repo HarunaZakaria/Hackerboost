0acf2127862929f81313e4ed4b26dd991f90195b
Harunzy <Harunzy55@gmail.com>
2026-05-07 22:57:15 -0700

    add updates to stats

--- travel/src/components/Stats.jsx
+++ travel/src/components/Stats.jsx
@@ -1,7 +1,18 @@
-export default function Stats() {
+export default function Stats({ items }) {
+  //number of items
+  const numOfItems = items.length;
+
+  //number of  items packed
+  const itemsPacked = items.filter((item) => item.packed).length;
+
+  //percentsge of items packed
+  const percentage = Math.floor((itemsPacked / numOfItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed Y (z%).</em>
+      <em>
+        You have {numOfItems} items on your list, and you already packed{" "}
+        {itemsPacked} ({percentage}%).
+      </em>
     </footer>
   );
 }
