d2b6f0b4e5835a9f60233ea5111b099253819031
Harunzy <Harunzy55@gmail.com>
2026-05-08 14:14:41 -0700

    add a handleToggle function

--- accordion-app/src/components/AccordionItem.jsx
+++ accordion-app/src/components/AccordionItem.jsx
@@ -2,16 +2,20 @@ import { useState } from "react";
 
 export default function AccordionItem({ number, title, text }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
   return (
     <div className="item">
       <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">{isOpen ? "-" : "+"}</p>
+      <p className="icon" onClick={handleToggle}>{isOpen ? "-" : "+"}</p>
       {isOpen && (
         <div className="content-box">
           <p>{text}</p>
         </div>
-      ) }
+      )}
     </div>
   );
 }
