e0fbe2dc00eb3bc5bd1d6e55fcc888892b4872d6
Harunzy <Harunzy55@gmail.com>
2026-05-07 17:34:36 -0700

    add final solution

--- student-directory/src/components/ProfileCard.jsx
+++ student-directory/src/components/ProfileCard.jsx
@@ -4,13 +4,15 @@ export default function ProfileCard({ student, onToggle }) {
   const { name, track, bio, skillLevel, isActive } = student;
   return (
     <div className="student">
-      <h2>{name}</h2>
+      <h2 className="name">{name}</h2>
       <h3>{track}</h3>
       <p>{bio}</p>
-      <p>{skillLevel}</p>
-      <button onClick={() => onToggle(student.id)}>
-        {isActive ? "Activate" : "Deactivate"}
-      </button>
+      <div className="stud-skill-btn">
+        <p>{skillLevel}</p>
+        <button onClick={() => onToggle(student.id)}>
+          {isActive ? "Activate" : "Deactivate"}
+        </button>
+      </div>
     </div>
   );
 }
--- student-directory/src/components/ProfileList.jsx
+++ student-directory/src/components/ProfileList.jsx
@@ -3,11 +3,13 @@ import ProfileCard from "./ProfileCard";
 export default function ProfileList({ students, onToggle }) {
   return (
     <>
-      {students.map((student) => (
-        <div key={student.id} className="students">
-          <ProfileCard student={student} onToggle={onToggle}/>
-        </div>
-      ))}
+      <div className="students">
+        {students.map((student) => (
+          <div key={student.id}>
+            <ProfileCard student={student} onToggle={onToggle} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
