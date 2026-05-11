d9f3bbaa2c9ad3ba3e9392d04843618d93d8d8c7
Harunzy <Harunzy55@gmail.com>
2026-05-10 17:55:24 -0700

    add update

--- student-directory/src/App.jsx
+++ student-directory/src/App.jsx
@@ -2,60 +2,12 @@ import { useState } from "react";
 import Header from "./components/Header";
 import ProfileList from "./components/ProfileList";
 import Footer from "./components/Footer";
-
+import students from "./data";
+import studentsData from "./data";
 function App() {
   // students Data
 
-  const [students, setStudents] = useState([
-    {
-      id: 1,
-      name: "Amina",
-      track: "Frontend Development",
-      bio: "Learning React and building interfaces",
-      skillLevel: "Beginner",
-      isActive: true,
-    },
-    {
-      id: 2,
-      name: "Kofi",
-      track: "Backend Development",
-      bio: "Working with APIs and databases",
-      skillLevel: "Intermediate",
-      isActive: false,
-    },
-    {
-      id: 3,
-      name: "Zakaria",
-      track: "Backend Development",
-      bio: "Working with APIs and databases",
-      skillLevel: "Intermediate",
-      isActive: true,
-    },
-    {
-      id: 4,
-      name: "Haruna",
-      track: "FullStack Development",
-      bio: "Learning React and building interfaces and Working with APIs and databases",
-      skillLevel: "Beginner",
-      isActive: true,
-    },
-    {
-      id: 5,
-      name: "Dauda",
-      track: "UI/UX Designer",
-      bio: "Working user experience and interface design",
-      skillLevel: "Intermediate",
-      isActive: false,
-    },
-    {
-      id: 6,
-      name: "Muazu",
-      track: "Project Manager",
-      bio: "Working with designers and developers to bring projects to live",
-      skillLevel: "Beginner",
-      isActive: false,
-    },
-  ]);
+  const [students, setStudents] = useState(studentsData);
 
   ///toggle effect
   function handleToggle(id) {
