76f2d84cc484c4249f8cab7ff0cedaa8c25a5d98
Harunzy <Harunzy55@gmail.com>
2026-06-13 16:09:53 -0700

    add

--- reaVote/src/components/PollCard.jsx
+++ reaVote/src/components/PollCard.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 
 function PollCard({ polls, setPolls }) {
+  //handle vote click
+  function handleVote(pollId, optionId) {
+    setPolls((prevPolls) =>
+      prevPolls.map((poll) =>
+        poll.id === pollId
+          ? {
+              ...poll,
+              options: poll.options.map((option) =>
+                option.id === optionId
+                  ? { ...option, votes: option.votes + 1 }
+                  : option,
+              ),
+            }
+          : poll,
+      ),
+    );
+  }
   return (
     <div className="poll-card">
+      <h1>Questions</h1>
       {polls.map((poll) => (
         <ul key={poll.id}>
-          <h2>{poll.question}</h2>
+          <h3>{poll.question}</h3>
           {poll.options.map((option) => (
-            <li key={option.id}> {option.label}</li>
+            <li key={option.id}>
+              {option.label}
+              <button onClick={() => handleVote(poll.id, option.id)}>
+                Vote
+              </button>
+            </li>
           ))}
         </ul>
       ))}
