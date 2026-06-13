f89c932f7977a585c2f8ade118983537da6f4dc4
Harunzy <Harunzy55@gmail.com>
2026-06-13 12:48:59 -0700

    add data.js

---
+++ reaVote/src/data.js
@@ -0,0 +1,21 @@
+const polls = [
+  {
+    id: 1,
+    question: "Best Frontend Framework?",
+    options: [
+      { id: 1, label: "React", votes: 12 },
+      { id: 2, label: "Vue", votes: 80 },
+      { id: 3, label: "Angula", votes: 36 },
+    ],
+  },
+  {
+    id: 2,
+    question: "Best Backend Languages?",
+    options: [
+      { id: 1, label: "Python", votes: 2 },
+      { id: 2, label: "Go", votes: 20 },
+      { id: 3, label: "Java", votes: 13 },
+    ],
+  },
+];
+export default polls;
