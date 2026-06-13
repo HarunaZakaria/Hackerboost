9d0b4c1942ad50c8a6e304e5775879ba71f1eed2
Harunzy <Harunzy55@gmail.com>
2026-06-13 13:13:00 -0700

    add

--- reaVote/src/components/LeaderBoard.jsx
+++ reaVote/src/components/LeaderBoard.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-function LeaderBoard() {
+function LeaderBoard({ polls }) {
   return (
     <div className="leaderboard">
       <h1>Leaderboard</h1>
-      <p>leaders section</p>
+
+      {polls.map((poll) => (
+        <ul key={poll.id}>
+          <h2>{poll.role}</h2>
+          {poll.options.map((option) => (
+            <li key={option.id}>
+              {option.label} - {option.votes}
+            </li>
+          ))}
+        </ul>
+      ))}
     </div>
   );
 }
