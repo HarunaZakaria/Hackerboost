80726b427116b340d1e661f9e6880ba3f3187565
Harunzy <Harunzy55@gmail.com>
2026-07-02 17:38:15 -0700

    add a focus to search

--- usepopcorn/src/components/navbar/Seach.jsx
+++ usepopcorn/src/components/navbar/Seach.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    inputEl.current.focus();
+  }, []);
   return (
     <input
+      ref={inputEl}
       className="search"
       type="text"
       placeholder="Search movies..."
