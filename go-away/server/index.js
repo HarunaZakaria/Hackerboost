b6933cd3d0884a32e879bf6bcfd9ce4662b59dca
Harunzy <Harunzy55@gmail.com>
2026-08-10 14:01:45 -0700

    add database connection

--- go-away/server/index.js
+++ go-away/server/index.js
@@ -1,6 +1,18 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const port = process.env.port || 5000;
 const app = express();
-const port = 5000;
+//const port = 5000;
+
+//connect to mongoDB
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ Connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("Hello");
