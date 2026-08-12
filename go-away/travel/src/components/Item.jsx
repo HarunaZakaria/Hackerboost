60ae48c43b2d7dee768cfde725bb68e52a05ac6b
Harunzy <Harunzy55@gmail.com>
2026-08-11 17:12:39 -0700

    update id to _id

--- go-away/travel/src/components/Item.jsx
+++ go-away/travel/src/components/Item.jsx
@@ -5,12 +5,12 @@ export default function Item({ item, deleteItem, onToggle }) {
       <input
         type="checkbox"
         checked={item.packed}
-        onChange={() => onToggle(item.id)}
+        onChange={() => onToggle(item._id)}
       />
       <span style={packed ? { textDecoration: "line-through" } : {}}>
         {quantity} {description}
       </span>
-      <button onClick={() => deleteItem(item.id)} className="button">
+      <button onClick={() => deleteItem(item._id)} className="button">
         X
       </button>
     </li>
