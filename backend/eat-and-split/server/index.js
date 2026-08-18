dba40ec397e1680993b3d780986f3e17a8e1646f
Harunzy <Harunzy55@gmail.com>
2026-08-17 21:25:28 -0700

    add api routes

--- backend/eat-and-split/server/index.js
+++ backend/eat-and-split/server/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import friendsModel from "./src/models/friends";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,73 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+//get all friends api
+app.get("/api/friends", async (req, res) => {
+  try {
+    const friends = await friendsModel.find();
+    if (!friends) {
+      res.status(404).json({
+        status: "fail",
+        message: "Friends not found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: { friends },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+});
+
+// add new friend
+app.post("/api/friends", async (req, res) => {
+  try {
+    const friend = await friendsModel.create({
+      name: req.body.name,
+      image: `https://i.pravatar.cc/100?u=${Date.now()}`, // Unique avatar URL
+    });
+    res.status(201).json({
+      status: "Success",
+      data: { friend },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Error",
+      message: err.message,
+    });
+  }
+});
+
+//update balance
+app.patch("/api/friends/:id/balace", async (req, res) => {
+  try {
+    const friend = await friendsModel.findByIdAndUpdate(
+      req.params.id,
+      { $inc: { balace: req.body.amount } },
+      { new: true, runValidators: true },
+    );
+    if (!friend) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Friend not found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      data: { friend },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 });
 app.listen(port, () => {
   console.log(`app running on port: ${port}`);
