b3c4455d00e49c4a2bf452d47ee2a00aa9482823
Harunzy <Harunzy55@gmail.com>
2026-08-17 17:39:23 -0700

    add a database connection

--- backend/eat-and-split/server/index.js
+++ backend/eat-and-split/server/index.js
@@ -1,7 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
 const port = 5000;
+
+//connect to mongoDB
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ Connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
