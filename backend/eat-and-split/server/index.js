555e5a19cab4160cdae895d1531703ea205c2171
Harunzy <Harunzy55@gmail.com>
2026-08-18 14:07:03 -0700

    add

--- backend/eat-and-split/server/index.js
+++ backend/eat-and-split/server/index.js
@@ -5,7 +5,7 @@ import friendsModel from "./src/models/friends.js";
 
 dotenv.config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middlewares
 app.use(express.json());
