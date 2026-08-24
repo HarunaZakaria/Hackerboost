087698060937c40b43af34e22c0a047990120afa
Harunzy <Harunzy55@gmail.com>
2026-08-24 15:02:35 -0700

    add

--- go-away/travel/src/components/Form.jsx
+++ go-away/travel/src/components/Form.jsx
@@ -2,23 +2,36 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Form({ items, setItems }) {
-
   const [newItem, setNewItem] = useState({
-    id: uuidv4(),
+    //id: uuidv4(),
     quantity: 1,
     description: "",
   });
 
   //handle submit
-  function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setItems((items) => [...items, newItem]);
-    setNewItem({
-      id: uuidv4(),
-      quantity: 1,
-      description: "",
-    });
-  }
+    try {
+      const response = await fetch("http://localhost:5000/api/items", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newItem),
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message || "Something went wrong!");
+      }
+      setItems((items) => [...items, data.data]);
+      setNewItem({
+        quantity: 1,
+        description: "",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
       <form onSubmit={handleSubmit}>
