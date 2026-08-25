b7f3cfd50e25eba8a1be81e9ef35acec3ba3f87c
Harunzy <Harunzy55@gmail.com>
2026-08-25 13:19:41 -0700

    fetch data from API

--- backend/eat-and-split/eat-n-split/src/App.jsx
+++ backend/eat-and-split/eat-n-split/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "./components/Button";
 import FormAddFriend from "./components/FormAddFriend";
 import FormSplitBill from "./components/FormSplitBill";
@@ -7,9 +7,28 @@ import initialFriends from "./data";
 import Header from "./components/Header";
 
 function App() {
-  const [friends, setFriends] = useState(initialFriends);
+  const [friends, setFriends] = useState([]);
   const [showAddFriend, setShowAddFriend] = useState(false);
   const [selectedFriend, setSelectedFriend] = useState(null);
+
+  // fetch friends from the server
+  useEffect(function () {
+    async function fetchItems() {
+      try {
+        const response = await fetch("http://localhost:5000/api/friends");
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await response.json();
+        setFriends(data.data);
+        console.log(data.data);
+      } catch (error) {
+        setErro(error.message);
+      }
+    }
+
+    fetchItems();
+  }, []);
 
   //handle add friends click
   function handleAddFriend(friend) {
